0e99bdc1fe2581232436054d63f7b4a2b32caa1a
EssiKarj <essi-karj@icloud.com>
2022-04-04 09:49:55 +0100

    link direction changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ const Welcome = () => {
 
   return (
     <div className='welcome-container'>
-      <p className='left-aligned'><strong>Welcome.</strong><br /> I am Essi, a software developer from London, and this is my portfolio.</p>
+      <p className='left-aligned'><strong>Welcome.</strong><br /> I am Essi, a junior software developer from London, and this is my portfolio.</p>
 
       <p className='right-aligned'>Scroll down to see what I have been up to.</p>
     </div>
